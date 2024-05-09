9a2d4baa5e07dc4aecc2e16ec864b2e5d234e2ae
amagnemite <41340875+amagnemite@users.noreply.github.com>
2024-05-09 00:33:31 -0400

    header is now specific and use ints for comparisons

--- src/ninoscript/ScriptReader.java
+++ src/ninoscript/ScriptReader.java
@@ -15,15 +15,12 @@ public class ScriptReader {
 	private static final byte Z = 0x5A;
 	private static final byte m = 0x6D;
 	private static final byte p = 0x70;
-	private static final short FULLWIDTH1 = 0x81;
-	private static final byte FULLWIDTHMARKER = 0x5C; //is \
-	
-	private byte[] fullFileBytes;
 	
 	private String scriptName;
 	private List<BlockData> blockList = new ArrayList<BlockData>();
 	
 	public ScriptReader(File file) {
+		byte[] fullFileBytes;
 		try {
 			FileInputStream input = new FileInputStream(file);
 			fullFileBytes = new byte[input.available()];
@@ -38,7 +35,7 @@ public class ScriptReader {
 		}
 		
 		//magic word
-		if(fullFileBytes[0] != 0x0A && fullFileBytes[1] != 0x08 && fullFileBytes[2] != 0x01F) {
+		if(fullFileBytes[0] != 0x0A && fullFileBytes[1] != 0x08 && fullFileBytes[2] != 0x1F) {
 			return;
 		}
 		
@@ -57,110 +54,129 @@ public class ScriptReader {
 				case 0x00:
 					continue;
 				case 0x05:
+					//it seems that the max block length should be 255 bytes?
+					//longest so far is 207
+					//so 05 01-FF 00 01 01-FF 00
+					
+					//01-FF
 					if(fullFileBytes[i + 1] == 0x00) {
 						continue;
 					}
 					
-					int testLength = (fullFileBytes[i + 1] & 0xFF) | (fullFileBytes[i + 2] & 0xFF) << 8;
-					int testLength2 = (fullFileBytes[i + 4] & 0xFF) | (fullFileBytes[i + 5] & 0xFF) << 8;
-					
-					if(testLength < testLength2) {
+					//00
+					if(fullFileBytes[i + 2] != 0x00) {
 						continue;
 					}
 					
-					if(fullFileBytes[i + 3] == 0x01) {
-						BlockData data = parseBlock(i);
-						
-						if(data != null) {
-							blockList.add(data);
-							i += (fullFileBytes[i + 1] & 0xFF) | (fullFileBytes[i + 2] & 0xFF) << 8;
-						}
+					//01
+					if(fullFileBytes[i + 3] != 0x01) {
+						continue;
 					}
+					
+					//01-FF
+					if(fullFileBytes[i + 4] == 0x00) {
+						continue;
+					}
+					
+					//00
+					if(fullFileBytes[i + 5] != 0x00) {
+						continue;
+					}
+					
+					//there's blocks with a leading 04 that otherwise follow the header style
+					if(fullFileBytes[i - 1] == 0x04) {
+						continue;
+					}
+					
+					BlockData data = parseBlock(fullFileBytes, i);
+					
+					if(data != null) {
+						blockList.add(data);
+						i += data.getFullBlockLength();
+					}
+					break;
+				default:
 					break;
 			}
 		}
-		System.out.println(blockList.size());
+		//System.out.println(blockList.size());
 	}
 	
-	private BlockData parseBlock(int start) {
+	private BlockData parseBlock(byte[] fullFileBytes, int start) {
 		int shift = start; //starts at the 05 loc
 		byte[] textBytes;
 		byte[] speakerBytes = null;
 		int speakerLength = 0;
 		int speakerStart = 0;
-		//boolean escapeFullWidth = false;
 		String speakerBytesString = null;
 		
-		if(shift == 3498) {
-			System.out.println("funny block");
+		if(start == 31137) {
+			System.out.println("test");
 		}
+
+		int fullBlockLength = fullFileBytes[shift + 1] & 0xFF;
 		
-		int fullBlockLength = (fullFileBytes[shift + 1] & 0xFF) | (fullFileBytes[shift + 2] & 0xFF) << 8;
-		//things that don't have text will have super long fullblocklengths, but they should fail the check while iterating for chars
 		
-		if(shift + 2 + fullBlockLength > fullFileBytes.length || fullFileBytes[shift + 2 + fullBlockLength] > 0x0F) {
+		
+		/*
+		//if(shift + 2 + fullBlockLength > fullFileBytes.length || fullFileBytes[shift + 2 + fullBlockLength] > 0x0F) {
+		if(shift + 2 + fullBlockLength > fullFileBytes.length) {
+			System.out.println("test 1 " + fullFileBytes[start - 1]);
 			return null;
 		}
+		*/
+		/*
+		if(fullFileBytes[shift + 2 + fullBlockLength] > 0x0F) {
+			//System.out.println("test 2 " + fullBlockLength);
+			System.out.println("test 2 " + fullFileBytes[start - 1]);
+			//System.out.println("start " + start + " " + fullFileBytes[shift + 2 + fullBlockLength]);
+			return null;
+		}
+		*/
 		
 		shift += 4; //skip 01	
-		int textLength = (fullFileBytes[shift] & 0xFF) | (fullFileBytes[shift + 1] & 0xFF) << 8;
-		
-		if(textLength == 0) {
-			return null;
-		}
+		int textLength = fullFileBytes[shift] & 0xFF;
 		
 		shift += 2;
 		int textStart = shift;
 		
+		/*
 		if(fullFileBytes[textStart] < 0x19 || fullFileBytes[textStart] > 0x7F) {
 			return null;
 		}
+		*/
 		
 		textBytes = new byte[textLength];
 		for(int i = 0; i < textLength; i++) {
 			textBytes[i] = fullFileBytes[shift + i];
-				
-			/*
-			byte b = fullFileBytes[shift + i];
-			if(escapeFullWidth) {
-				//escaped a fullwidth, so just put it in here
-				textBytes[i] = fullFileBytes[shift + i];
-				escapeFullWidth = false;
-			}
-			else if((short) b == FULLWIDTH1) {
-				//fullwidth is marked by 0x81, which java doesn't accept as a byte so just swap it here
-				textBytes[i] = FULLWIDTHMARKER;
-				escapeFullWidth = true;
-			}
-			else if(b > 0x19 && b < 0x7F) { //is it a valid ascii char
-				textBytes[i] = fullFileBytes[shift + i];
-			}
-			else {
-				return null;
-			}
-			*/
 		}
 		
 		shift += textLength + PADDING1;
 		//starting at this point, blocks are variable
 		
 		while(shift < start + fullBlockLength) {
-			if(fullFileBytes[shift] < 0x0F) { //for now, hardcode possible interesting things
+			int currentByte = Byte.toUnsignedInt(fullFileBytes[shift]);
+			int nextByte = Byte.toUnsignedInt(fullFileBytes[shift + 1]);
+			int nextByte2 = Byte.toUnsignedInt(fullFileBytes[shift + 2]);
+			int nextByte3 = Byte.toUnsignedInt(fullFileBytes[shift + 3]);
+			
+			if(currentByte < 0x0F) { //for now, hardcode possible interesting things
 				if(shift + 1 >= start + fullBlockLength) { //end of block, ignore
 					break;
 				}
-				else if(fullFileBytes[shift] == 0x00) {
-					//also oliver seems to consistently have 00 xx 06, so possible other chars do too
-					if(fullFileBytes[shift + 1] == 0x00 && fullFileBytes[shift + 2] == 0x00) {
+				else if(currentByte == 0x00) {
+					//oliver seems to consistently have 00 xx 06, so possible other chars do too
+					if(nextByte == 0x00 && nextByte2 == 0x00) {
 						//sometimes speakers have 3 leading 00s, so this is a dumb way to avoid it
 						shift += 3;
 						continue;
 					}
 					else {
-						if(fullFileBytes[shift + 1] < 0x0F && fullFileBytes[shift + 2] < 0x0F && fullFileBytes[shift + 3] >= 0x21) {
+						if(nextByte < 0x05 && nextByte2 < 0x0F && nextByte3 >= 0x21) {
 							//there is a npc with 00 00 05
-							//2nd check is to dodge headers 
-							speakerLength = fullFileBytes[shift + 2] & 0xFF;
+							//00 00-04 00-0D [an ascii char] 
+							speakerLength = nextByte2;
+							//speakerLength = fullFileBytes[shift + 2] & 0xFF;
 							speakerStart = shift + 3;
 							speakerBytes = new byte[speakerLength];
 							for(int i = 0; i < speakerLength; i++) {
@@ -169,8 +185,8 @@ public class ScriptReader {
 							
 							//System.out.println(fullFileBytes[shift] + " " + fullFileBytes[shift + 1] + " " + fullFileBytes[shift + 2] +
 							//		" " + new String(speakerBytes, StandardCharsets.US_ASCII));
-							speakerBytesString = fullFileBytes[shift] + " " + fullFileBytes[shift + 1] + " " + fullFileBytes[shift + 2] +
-									" " + new String(speakerBytes, StandardCharsets.US_ASCII);
+							//speakerBytesString = fullFileBytes[shift] + " " + fullFileBytes[shift + 1] + " " + fullFileBytes[shift + 2] +
+							//		" " + new String(speakerBytes, StandardCharsets.US_ASCII);
 							
 							shift += 3 + speakerLength;
 						}
@@ -179,18 +195,19 @@ public class ScriptReader {
 						}
 					}
 				}
-				else if(fullFileBytes[shift + 1] == m) {
+				else if(nextByte == m) {
 					//likely model file
 					shift += 1 + (fullFileBytes[shift] & 0xFF);
 				}
-				else if(fullFileBytes[shift + 1] == p) {
+				else if(nextByte == p) {
 					//likely joint anim file
 					shift += 1 + (fullFileBytes[shift] & 0xFF);
 				}
 				else { //all we know is sub 0x0F val
-					if(fullFileBytes[shift + 1] < 0x0F && fullFileBytes[shift + 2] < 0x0F && fullFileBytes[shift + 3] >= 0x21) {
+					if(nextByte < 0x05 && nextByte2 < 0x0F && nextByte3 >= 0x21) {
+						//00 00-04 00-0D [an ascii char]
 						//likely a speaker header
-						speakerLength = fullFileBytes[shift + 2] & 0xFF;
+						speakerLength = nextByte2;
 						speakerStart = shift + 3;
 						speakerBytes = new byte[speakerLength];
 						for(int i = 0; i < speakerLength; i++) {
@@ -199,8 +216,8 @@ public class ScriptReader {
 						
 						//System.out.println(fullFileBytes[shift] + " " + fullFileBytes[shift + 1] + " " + fullFileBytes[shift + 2] +
 						//		" " + new String(speakerBytes, StandardCharsets.US_ASCII));
-						speakerBytesString = fullFileBytes[shift] + " " + fullFileBytes[shift + 1] + " " + fullFileBytes[shift + 2] +
-								" " + new String(speakerBytes, StandardCharsets.US_ASCII);
+						//speakerBytesString = fullFileBytes[shift] + " " + fullFileBytes[shift + 1] + " " + fullFileBytes[shift + 2] +
+						//		" " + new String(speakerBytes, StandardCharsets.US_ASCII);
 						shift += 3 + speakerLength;
 					}
 					else {
@@ -218,6 +235,9 @@ public class ScriptReader {
 			System.out.println("");
 		}
 		*/
+		if(speakerBytes != null && Byte.toUnsignedInt(speakerBytes[0]) > 0x7F) {
+			System.out.println(speakerStart + " " + new String(speakerBytes, StandardCharsets.US_ASCII));
+		}
 		
 		return new BlockData(fullBlockLength, textLength, textStart, speakerStart, speakerLength, textBytes, speakerBytes, speakerBytesString);
 	}
